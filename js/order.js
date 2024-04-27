5a02b28631219d18618dfdb7ba220148287f0fc7
Sagar Bodlekar <sagarbodlekar@gmail.com>
2024-04-27 11:47:24 +0530

    Fresh First Commit in this branch to multiple type changes

--- js/order.js
+++ js/order.js
@@ -10,3 +10,9 @@
 //               console.log("running123")
 //               console.log(t);
 // });
+
+$(function () {
+    $(".fold-table tr.view").on("click", function () {
+        $(this).toggleClass("open").next(".fold").toggleClass("open");
+    });
+});
